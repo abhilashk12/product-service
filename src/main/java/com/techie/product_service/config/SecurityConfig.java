5ccb7a843e6ab4d1bf9dc40067e92d1ec7ac8fcc
Abhilash Kelageri <abhiabhilash74658.com>
2026-03-12 21:29:01 +0530

    updated yml

--- src/main/java/com/techie/product_service/config/SecurityConfig.java
+++ src/main/java/com/techie/product_service/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/products/**").permitAll()
+                        .requestMatchers("/products/**", "/actuator/**" ).permitAll()
                         .anyRequest().authenticated()
                 );
 
