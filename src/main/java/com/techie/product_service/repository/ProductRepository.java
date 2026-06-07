015c83a18b432471ba938a278fe08e9f3f65289f
abhilashk12 <abhiabhilash74658@gmail.com>
2026-06-07 19:07:36 +0530

    Add findByNameDummy method to ProductRepository

--- src/main/java/com/techie/product_service/repository/ProductRepository.java
+++ src/main/java/com/techie/product_service/repository/ProductRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByNameContainingIgnoreCase(String name);
+
+    List<Product> findByNameDummy(String name);
 }
